463d7c18e1e62da44245a0e74b7a57e17b717d0e
Shailesh Baldaniya <shaileshbaldaniya351@gmail.com>
2020-10-03 21:30:38 +0530

    Add Clockwise and Anticlockwise Spin Animation

--- App.js
+++ App.js
@@ -1,14 +1,61 @@
-import React from 'react';
-import {SafeAreaView, StyleSheet, StatusBar} from 'react-native';
+import React, {useRef} from 'react';
+import {
+  Animated,
+  SafeAreaView,
+  StyleSheet,
+  StatusBar,
+  Button,
+  Easing,
+  View,
+} from 'react-native';
 import YinYang from './YinYang';
 
 const App = () => {
-  console.log('Shell');
+  const anim = useRef(new Animated.Value(0)).current;
+
+  const clockWise = () => {
+    Animated.timing(anim).stop();
+    anim.setValue(0);
+    Animated.loop(
+      Animated.timing(anim, {
+        toValue: 1,
+        duration: 2000,
+        easing: Easing.linear,
+        useNativeDriver: true,
+      }),
+    ).start();
+  };
+
+  const antiClockWise = () => {
+    Animated.timing(anim).stop();
+    anim.setValue(1);
+    Animated.loop(
+      Animated.timing(anim, {
+        toValue: 0,
+        duration: 2000,
+        easing: Easing.linear,
+        useNativeDriver: true,
+      }),
+    ).start();
+  };
+
+  const spin = anim.interpolate({
+    inputRange: [0, 1],
+    outputRange: ['0deg', '360deg'],
+  });
+
   return (
     <>
       <StatusBar barStyle="dark-content" />
       <SafeAreaView style={styles.safeArea}>
-        <YinYang size={300} />
+        <Animated.View style={[styles.safeArea, {transform: [{rotate: spin}]}]}>
+          <YinYang size={300} />
+        </Animated.View>
+
+        <View>
+          <Button title="Clock Wise" onPress={clockWise} />
+          <Button title="Anti Clock Wise" onPress={antiClockWise} />
+        </View>
       </SafeAreaView>
     </>
   );
