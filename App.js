a37dc4775c930ebe922a4abc9e61898228877ac0
Shailesh Baldaniya <shaileshbaldaniya351@gmail.com>
2020-10-09 21:23:27 +0530

    Handle Rotation Change for MaxSize

--- App.js
+++ App.js
@@ -9,12 +9,11 @@ import {
   View,
 } from 'react-native';
 import Slider from '@react-native-community/slider';
-import {Dimensions} from 'react-native';
 import YinYang from './YinYang';
 
 const App = () => {
   const anim = useRef(new Animated.Value(0)).current;
-  const maxSize = Dimensions.get('window').width;
+  const [maxSize, setMaxSize] = useState(1);
   const [size, setSize] = useState(300);
 
   const clockWise = () => {
@@ -48,11 +47,22 @@ const App = () => {
     outputRange: ['0deg', '360deg'],
   });
 
+  const onLayout = (event) => {
+    let {width, height} = event.nativeEvent.layout;
+    const newMaxSize = Math.min(width, height);
+    if (maxSize !== newMaxSize) {
+      setSize(newMaxSize / 2);
+    }
+    setMaxSize(newMaxSize);
+  };
+
   return (
     <>
       <StatusBar barStyle="dark-content" />
       <SafeAreaView style={styles.safeArea}>
-        <Animated.View style={[styles.safeArea, {transform: [{rotate: spin}]}]}>
+        <Animated.View
+          style={[styles.safeArea, {transform: [{rotate: spin}]}]}
+          onLayout={(event) => onLayout(event)}>
           <YinYang size={size} />
         </Animated.View>
 
@@ -66,9 +76,9 @@ const App = () => {
             onValueChange={(value) => {
               setSize(value);
             }}
-            value={Math.round((maxSize - 16) / 2)}
+            value={Math.round(maxSize / 2)}
             minimumValue={0}
-            maximumValue={Math.round(maxSize - 16)}
+            maximumValue={Math.round(maxSize)}
             minimumTrackTintColor="#000000"
             maximumTrackTintColor="#EEEEEE"
           />
